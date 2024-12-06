b87726ccaac69f194d3fd19a0f3768ace152112c
Mark Eggenstein <mark.eggenstein@eggnstone.com>
2024-12-06 14:25:55 +0700

    Updated lint rules.

--- lib/eggnstone_google_crashlytics.dart
+++ lib/eggnstone_google_crashlytics.dart
@@ -1,5 +1,5 @@
 /// eggnstone_google_crashlytics
-library eggnstone_google_crashlytics;
+library;
 
 export 'src/GoogleCrashlyticsNonWebService.dart' if (dart.library.html) 'src/GoogleCrashlyticsWebService.dart';
 export 'src/IGoogleCrashlyticsService.dart';
